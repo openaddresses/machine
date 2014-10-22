14ab9638565be49df3c873efcfa883f3a5b128eb
Michal Migurski <mike@teczno.com>
2014-10-21 23:56:58 -0700

    Added a dumb little timer thread

--- parallel.py
+++ parallel.py
@@ -4,21 +4,31 @@ from logging import getLogger, FileHandler, StreamHandler, Formatter, DEBUG
 from multiprocessing import cpu_count
 from argparse import ArgumentParser
 from openaddr import conform
+from time import sleep
 
-def run_thread(lock, source_files, destination_files):
+def run_conform(lock, source_files, destination_files):
     '''
     '''
     while True:
         with lock:
             if not source_files:
                 return
-            path = source_files.pop()
+            path = source_files.pop(0)
     
         getLogger('openaddr').info(path)
         csv_path = conform(path, 'out')
         
         with lock:
             destination_files[path] = csv_path
+
+def run_timer(source_files, interval):
+    '''
+    '''
+    sleep(interval)
+
+    while source_files:
+        getLogger('openaddr').debug('{0} source files remain'.format(len(source_files)))
+        sleep(interval)
 
 def setup_logger(logfile):
     ''' Set up logging stream and optional file for 'openaddr' logger.
@@ -45,7 +55,7 @@ if __name__ == '__main__':
 
     source_files = [
         '/var/opt/openaddresses/sources/us-ca-san_francisco.json',
-        #'/var/opt/openaddresses/sources/us-ca-alameda_county.json',
+        '/var/opt/openaddresses/sources/us-ca-alameda_county.json',
         '/var/opt/openaddresses/sources/us-ca-oakland.json',
         '/var/opt/openaddresses/sources/us-ca-berkeley.json'
         ]
@@ -53,8 +63,10 @@ if __name__ == '__main__':
     destination_files = OrderedDict()
     args = Lock(), source_files, destination_files
 
-    threads = [Thread(target=run_thread, args=args)
+    threads = [Thread(target=run_conform, args=args)
                for i in range(cpu_count() + 1)]
+    
+    threads.append(Thread(target=run_timer, args=(source_files, 15)))
 
     for thread in threads:
         thread.start()
