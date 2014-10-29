2f12f166b0509cbda54494acce3fb278489ce36d
Michal Migurski <mike@teczno.com>
2014-10-28 22:59:35 -0700

    Corrected more bugs in fresh system install

--- chef/openaddr-conform/recipes/default.rb
+++ chef/openaddr-conform/recipes/default.rb
@@ -1,9 +1,15 @@
+package 'gdal-bin'
 package 'nodejs'
 package 'npm'
 
 git '/var/opt/openaddresses-conform' do
   repository 'https://github.com/openaddresses/openaddresses-conform.git'
   reference 'ubuntu-bugfixes'
+end
+
+# One package used here is tetchy about node vs. nodejs
+link '/usr/bin/node' do
+  to '/usr/bin/nodejs'
 end
 
 execute 'npm install' do
