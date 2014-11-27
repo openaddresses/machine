120aa0e76769fcd4bc3b19badb1bc065145254e3
Michal Migurski <mike@teczno.com>
2014-11-26 18:50:48 -0800

    Moved iter.py to openaddr.sample

--- openaddr/sample.py
+++ openaddr/sample.py
@@ -1,12 +1,7 @@
-import ijson, csv, pprint
+import ijson, csv, pprint, httplib
 from itertools import chain
 
-with open('iter.csv', 'w') as output, open('iter.geojson') as input:
-    out = csv.writer(output)
-    for (i, (p, e, v)) in enumerate(ijson.parse(input)):
-        out.writerow((i, p, e, v))
-
-def build_value(data):
+def _build_value(data):
     ''' Build a value (number, array, whatever) from an ijson stream.
     '''
     for (prefix, event, value) in data:
@@ -14,16 +9,16 @@ def build_value(data):
             return value
         
         elif event == 'start_array':
-            return build_list(data)
+            return _build_list(data)
         
         elif event == 'start_map':
-            return build_map(data)
+            return _build_map(data)
         
         else:
             # MOOP.
             raise ValueError((prefix, event, value))
 
-def build_list(data):
+def _build_list(data):
     ''' Build a list from an ijson stream.
     
         Stop when 'end_array' is reached.
@@ -35,13 +30,13 @@ def build_list(data):
             break
         
         else:
-            # let build_value handle the array item.
+            # let _build_value() handle the array item.
             _data = chain([(prefix, event, value)], data)
-            output.append(build_value(_data))
+            output.append(_build_value(_data))
     
     return output        
 
-def build_map(data):
+def _build_map(data):
     ''' Build a dictionary from an ijson stream.
     
         Stop when 'end_map' is reached.
@@ -53,18 +48,13 @@ def build_map(data):
             break
         
         elif event == 'map_key':
-            output[value] = build_value(data)
+            output[value] = _build_value(data)
         
         else:
             # MOOP.
             raise ValueError((prefix, event, value))
     
     return output
-
-file = open('iter.geojson')
-data = ijson.parse(file)
-
-pprint.pprint(build_value(data))
 
 def sample_geojson(stream, max_features=2):
     '''
@@ -96,20 +86,15 @@ def sample_geojson(stream, max_features=2):
                     if event == 'end_array' or len(features) == max_features:
                         break
                 
-                    # let build_value handle the feature.
+                    # let _build_value() handle the feature.
                     _data = chain([(prefix, event, value)], data)
-                    features.append(build_value(_data))
+                    features.append(_build_value(_data))
 
                 return dict(type='FeatureCollection', features=features)
     
     raise ValueError()
 
-file = open('iter.geojson')
-print sample_geojson(file)
-
 # http://s3.amazonaws.com/data.openaddresses.io/20141122/us-fl-palm_beach.json
-import httplib
-
 conn = httplib.HTTPConnection('s3.amazonaws.com')
 conn.request('GET', '/data.openaddresses.io/20141122/us-fl-palm_beach.json')
 resp = conn.getresponse()
