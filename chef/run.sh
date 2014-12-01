99c432f4d0bda6b2f6c3c64ac70e645379e693d7
Michal Migurski <mike@teczno.com>
2014-11-30 19:52:13 -0800

    Backdate Ruby for Ubuntu 12.04 on Travis

--- chef/run.sh
+++ chef/run.sh
@@ -1,11 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash -e
 #
 # Install the chef ruby gem if chef-solo is not in the path.
 # This script is safe to run multiple times.
 #
 if [ ! `which chef-solo` ]; then
-    apt-get install -y build-essential ruby ruby-dev
-    gem install chef ohai --no-rdoc --no-ri
+    release=`lsb_release -r`
+    if [ "$release" != "${release/12.04/}" ]; then
+        # Ruby 1.9.3 install provided for Ubuntu 12.04
+        apt-get install -y build-essential ruby1.9.3
+        gem1.9.3 install chef ohai --no-rdoc --no-ri
+    else
+        # Otherwise, assume Ubuntu ~14+
+        apt-get install -y build-essential ruby ruby-dev
+        gem install chef ohai --no-rdoc --no-ri
+    fi
 fi
 
 cd `dirname $0`
