7a064b8bd90afb3109c7a5f057d652788ab96111
Michal Migurski <mike@teczno.com>
2014-12-02 00:45:17 -0800

    Moved user data script to its own template file

--- openaddr/run.py
+++ openaddr/run.py
@@ -1,5 +1,6 @@
 from os import environ
 from logging import getLogger
+from os.path import join, dirname
 from argparse import ArgumentParser
 from time import sleep
 
@@ -33,33 +34,8 @@ def main():
     args = parser.parse_args()
     jobs.setup_logger(None)
     
-    user_data = '''#!/bin/sh -ex
-apt-get update -y
-apt-get install -y git apache2
-
-echo 'cloning' > /var/www/html/state.txt
-git clone https://github.com/openaddresses/machine /tmp/machine
-
-echo 'installing' > /var/www/html/state.txt
-apt-get install -y build-essential ruby ruby-dev
-gem install ohai -v 7.4.0 --no-rdoc --no-ri
-gem install chef --no-rdoc --no-ri
-/tmp/machine/chef/run.sh
-
-echo 'processing' > /var/www/html/state.txt
-openaddr-process -a {access_key} -s {secret_key} -l log.txt {bucketname}
-
-echo 'killing' > /var/www/html/state.txt
-python <<KILL
-
-from urllib import urlopen
-from boto.ec2 import EC2Connection
-
-instance = urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().strip()
-EC2Connection('{ec2_access_key}', '{ec2_secret_key}').terminate_instances(instance)
-
-KILL
-'''.format(**args.__dict__)
+    with open(join(dirname(__file__), 'templates', 'user-data.sh')) as file:
+        user_data = file.read().format(**args.__dict__)
     
     getLogger('openaddr').info('Prepared {} bytes of instance user data\n\n{}'.format(len(user_data), user_data))
 
