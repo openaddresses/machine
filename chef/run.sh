03fa4c2fe0a6c5c66ef2bbdf1b30d89b6d2f814e
Michal Migurski <mike@teczno.com>
2014-12-02 18:59:51 -0800

    Removed annoying chef/ohai dependency workaround
    See https://github.com/opscode/chef/issues/2517

--- chef/run.sh
+++ chef/run.sh
@@ -5,7 +5,7 @@
 #
 if [ ! `which chef-solo` ]; then
     apt-get install -y build-essential ruby ruby-dev
-    gem install chef ohai --no-rdoc --no-ri
+    gem install chef --no-rdoc --no-ri
 fi
 
 cd `dirname $0`
--- openaddr/templates/user-data.sh
+++ openaddr/templates/user-data.sh
@@ -6,9 +6,6 @@ echo 'cloning' > /var/www/html/state.txt
 git clone https://github.com/openaddresses/machine /tmp/machine
 
 echo 'installing' > /var/www/html/state.txt
-apt-get install -y build-essential ruby ruby-dev # For now install chef here instead of run.sh,
-gem install ohai -v 7.4.0 --no-rdoc --no-ri      # because of 2014-12-01 dependency conflict:
-gem install chef --no-rdoc --no-ri               # https://github.com/opscode/chef/issues/2517
 /tmp/machine/chef/run.sh
 
 echo 'processing' > /var/www/html/state.txt
