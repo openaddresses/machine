6f9161bc8506f6d542f8d4e9f5181d9c86822b08
Michal Migurski <mike@teczno.com>
2015-01-26 21:52:10 -0800

    Removed all Node recipes

--- chef/openaddr-download/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-include_recipe 'node'
-
-execute 'npm install -g openaddresses-download' do
-  not_if 'which openaddresses-download'
-end
--- chef/openaddr-source/recipes/default.rb
+++ chef/openaddr-source/recipes/default.rb
@@ -1,11 +1,4 @@
-include_recipe 'node'
-
 git '/var/opt/openaddresses' do
   repository 'https://github.com/openaddresses/openaddresses.git'
   reference 'master'
-end
-
-execute 'npm install' do
-  cwd '/var/opt/openaddresses'
-  environment({'HOME' => '/', 'PATH' => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'})
 end
