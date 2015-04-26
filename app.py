f37bd5bb696e0a0497fcea3ea419568f94b94298
Michal Migurski <mike@teczno.com>
2015-04-25 17:11:57 -0700

    Added webhook output

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from sys import stderr
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return 'Yo.'
+
+@app.route('/hook', methods=['GET', 'POST'])
+def hook():
+    print >> stderr, request.method
+    print >> stderr, request.headers
+    print >> stderr, request.values
+    print >> stderr, repr(request.data)
+
     return 'Yo.'
 
 if __name__ == '__main__':
