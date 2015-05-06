46a1237ef45b5dec15e0cc8bb5572ed40b902b3a
Michal Migurski <mike@teczno.com>
2015-05-05 22:31:46 -0700

    Moved app under openaddr.ci module and tests under openaddr.tests

---
+++ test.py
@@ -0,0 +1,10 @@
+# coding=utf8
+"""
+Run Python test suite via the standard unittest mechanism.
+"""
+import unittest
+
+from openaddr.tests.ci import TestHook
+
+if __name__ == '__main__':
+    unittest.main()
