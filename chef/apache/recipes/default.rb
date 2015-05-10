72fddff261f25fe31f769e9b7bf9757d590f81aa
Michal Migurski <mike@teczno.com>
2015-05-10 10:55:05 -0700

    Added Apache proxy for Flask app

---
+++ chef/apache/recipes/default.rb
@@ -0,0 +1,23 @@
+package 'apache2'
+
+file '/etc/apache2/sites-available/webhook.conf' do
+  content <<-CONF
+<VirtualHost *:80>
+    <Location />
+        ProxyPass http://127.0.0.1:5000/
+        ProxyPassReverse http://127.0.0.1:5000/
+    </Location>
+    <Proxy http://127.0.0.1:5000/*>
+        Allow from all
+    </Proxy>
+</VirtualHost>
+CONF
+end
+
+execute 'a2enmod proxy'
+execute 'a2enmod proxy_http'
+
+execute 'a2ensite webhook'
+execute 'a2dissite 000-default'
+
+execute 'service apache2 reload'
