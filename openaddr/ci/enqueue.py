16844c1dcac8177bc86660a16fa959f3f3697e38
Michal Migurski <mike@teczno.com>
2015-06-17 22:05:24 -0700

    Collecting just current state of sources for now

--- openaddr/ci/enqueue.py
+++ openaddr/ci/enqueue.py
@@ -13,10 +13,11 @@ master_url = expand(commits_url, dict(sha=got['default_branch']))
 
 print('Getting {ref} branch {master_url}'.format(ref=got['default_branch'], **locals()))
 got = get(master_url, auth=auth).json()
-commit_sha = got['sha']
+commit_sha, commit_date = got['sha'], got['commit']['committer']['date']
 
 contents_url += '{?ref}' # So that we are consistently at the same commit.
 sources_urls = [expand(contents_url, dict(path='sources', ref=commit_sha))]
+sources_dict = dict()
 
 for sources_url in sources_urls:
     print('Getting sources {sources_url}'.format(**locals()))
@@ -31,14 +32,18 @@ for sources_url in sources_urls:
         if source['type'] != 'file':
             continue
         
-        _, ext = splitext(source['path'])
+        path_base, ext = splitext(source['path'])
         
         if ext == '.json':
-            print(source['url'])
+            key = relpath(path_base, 'sources')
+            val = dict(url=source['url'], sha=source['sha'], path=source['path'])
+            sources_dict[key] = val
+            continue
             
             source = get(source['url'], auth=auth).json()
             path = relpath(source['path'], 'sources')
             bytes = len(source['content'])
             
             print('{} bytes of {encoding}-encoded data in {}'.format(bytes, path, **source))
-    
+
+print(sources_dict)
