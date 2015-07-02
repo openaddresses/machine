3bc345cc2ab276cb589f1df0871942c1c7afa36f
Michal Migurski <mike@teczno.com>
2015-07-01 22:37:42 -0700

    Checking that copies are made of original runs, not copies

--- openaddr/tests/ci.py
+++ openaddr/tests/ci.py
@@ -707,16 +707,37 @@ class TestRuns (unittest.TestCase):
                 (count, ) = db.fetchone()
                 self.assertEqual(count, 2, 'There should have been two runs')
 
-                db.execute('''SELECT id, copy_of, state->>'nonce', source_path, source_id, source_data FROM runs
-                              ORDER BY datetime DESC LIMIT 1''')
+                db.execute('''SELECT id, copy_of, state->>'nonce' FROM runs
+                              ORDER BY id DESC LIMIT 1''')
 
-                ((second_run_id, second_copyof, second_nonce, db_source_path, db_source_id, db_source_data), ) = db.fetchall()
+                ((second_run_id, second_copyof, second_nonce), ) = db.fetchall()
                 self.assertNotEqual(second_run_id, first_run_id, 'The two runs should be distinct')
                 self.assertEqual(second_nonce, first_nonce, 'The two runs should share the same nonce')
                 self.assertEqual(second_copyof, first_run_id, 'The second run should be a copy of the first')
-                self.assertEqual(db_source_path, source_path)
-                self.assertEqual(db_source_id, source_id)
-                self.assertTrue(de64(bytes(db_source_data)).startswith('{'))
+
+            self.last_status_state = None
+
+            create_queued_job(task_Q, files, self.fake_job_template_url, self.fake_status_url)
+            pop_task_from_taskqueue(self.s3, task_Q, done_Q, due_Q, self.output_dir)
+            self.assertEqual(self.last_status_state, None, 'Should be nothing still')
+            
+            # Work done a third time!
+            pop_task_from_donequeue(done_Q, self.github_auth)
+            self.assertEqual(self.last_status_state, 'failure', 'Should be "failure" again')
+            
+            # Ensure that no new run was created
+            with done_Q as db:
+                db.execute('SELECT count(id) FROM runs')
+                (count, ) = db.fetchone()
+                self.assertEqual(count, 3, 'There should have been three runs')
+
+                db.execute('''SELECT id, copy_of, state->>'nonce' FROM runs
+                              ORDER BY id DESC LIMIT 1''')
+
+                ((third_run_id, third_copyof, third_nonce), ) = db.fetchall()
+                self.assertNotEqual(third_run_id, first_run_id, 'The two runs should be distinct')
+                self.assertEqual(third_nonce, second_nonce, 'The two runs should share the same nonce')
+                self.assertEqual(third_copyof, first_run_id, 'The third run should be a copy of the first')
 
     @patch('openaddr.jobs.JOB_TIMEOUT', new=timedelta(seconds=1))
     @patch('openaddr.ci.DUETASK_DELAY', new=timedelta(seconds=1))
