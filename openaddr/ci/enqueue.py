4ee76199ddffdc9718771ec7157945398b4aaa39
Michal Migurski <mike@teczno.com>
2015-07-08 22:56:28 -0700

    Enqueueing tasks only when there is room for them in the queue

--- openaddr/ci/enqueue.py
+++ openaddr/ci/enqueue.py
@@ -2,6 +2,9 @@ from os import environ
 from os.path import splitext, relpath
 from requests import get
 from uritemplate import expand
+from time import sleep
+
+from . import add_run, set_run, db_connect, db_queue, TASK_QUEUE
 
 auth = environ['GITHUB_TOKEN'], 'x-oauth-basic'
 start_url = 'https://api.github.com/repos/openaddresses/openaddresses'
@@ -47,3 +50,25 @@ for sources_url in sources_urls:
             print('{} bytes of {encoding}-encoded data in {}'.format(bytes, path, **source))
 
 print(sources_dict)
+
+with db_connect(environ['DATABASE_URL']) as conn:
+    queue = db_queue(conn, TASK_QUEUE)
+    for source in sources_dict.values():
+        while len(queue) >= 1:
+            print('sleeping because queue is', len(queue), 'long')
+            sleep(3)
+            
+        with queue as db:
+            more_source = get(source['url'], auth=auth).json()
+
+            task_data = dict(job_id=None, url=None, name=source['path'],
+                             content_b64=more_source['content'],
+                             file_id=source['sha'],
+                             commit_sha=commit_sha)
+            
+            task_id = queue.put(task_data)
+            print('enqueued task', task_id)
+        
+        #run_id = add_run(db)
+        #set_run(db, run_id, filename, file_id, content_b64, run_state, run_status,
+        #    job_id, worker_id, commit_sha)
