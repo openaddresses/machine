cb509c6cfd221d1306f57c30e792980a36f4552a
Michal Migurski <mike@teczno.com>
2015-08-05 21:33:31 -0700

    Revert "Transient local setup changes for testing"
    This reverts commit a15500d7ad898671ae3b81d1717082743c9ec53c.

--- openaddr/paths.py
+++ openaddr/paths.py
@@ -1,4 +1,3 @@
 cache = '/var/opt/openaddresses-cache'
 conform = '/var/opt/openaddresses-conform'
 sources = '/var/opt/openaddresses/sources'
-sources = '/Users/migurski/Sites/OpenAddresses-Machine/openaddr/tests/sources'
