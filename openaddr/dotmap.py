9369b12ff82864ebb85a560c4934bc309fd57e2e
Michal Migurski <mike-git@teczno.com>
2015-09-27 14:15:09 -0500

    Improved logging and output location

--- openaddr/dotmap.py
+++ openaddr/dotmap.py
@@ -4,17 +4,17 @@ import logging; _L = logging.getLogger('openaddr.dotmap')
 from .compat import standard_library
 
 from sys import stderr
-from os import environ
 from zipfile import ZipFile
 from os.path import splitext
-from tempfile import gettempdir
 from subprocess import Popen, PIPE
 from argparse import ArgumentParser
 from urllib.parse import urlparse, parse_qsl
+from tempfile import mkstemp, gettempdir
+from os import environ, close
 import json
 
 from .compat import csvIO, csvDictReader
-from .ci import db_connect, db_cursor
+from .ci import db_connect, db_cursor, setup_logger
 from .ci.objects import read_latest_set, read_completed_runs_to_date
 from . import iterate_local_processed_files
 
@@ -46,29 +46,38 @@ parser.add_argument('-d', '--database-url', default=environ.get('DATABASE_URL', 
 
 def main():
     args = parser.parse_args()
+    setup_logger(environ.get('AWS_SNS_ARN'))
     
     with connect_db(args.database_url) as conn:
         with db_cursor(conn) as db:
             set = read_latest_set(db, args.owner, args.repository)
             runs = read_completed_runs_to_date(db, set.id)
     
-    cmd = '/home/migurski/tippecanoe/tippecanoe', '-r', '2', \
-          '-l', 'openaddresses', '-X', '-n', 'OpenAddresses YYYY-MM-DD', '-f', \
-          '-t', gettempdir(), '-o', '/tmp/openaddresses.mbtiles'
+    handle, mbtiles_filename = mkstemp(prefix='oa-', suffix='.mbtiles')
+    close(handle)
+    
+    cmd = 'tippecanoe', '-r', '2', '-l', 'openaddresses', \
+          '-X', '-n', 'OpenAddresses YYYY-MM-DD', '-f', \
+          '-t', gettempdir(), '-o', mbtiles_filename
+    
+    _L.info('Running tippcanoe: {}'.format(' '.join(cmd)))
     
     tippecanoe = Popen(cmd, stdin=PIPE, bufsize=1)
-    zip_filenames = (fn for (_, fn, _) in iterate_local_processed_files(runs))
+    zip_details = ((source, filename) for (source, filename, _)
+                   in iterate_local_processed_files(runs))
     
-    for feature in stream_all_features(zip_filenames):
+    for feature in stream_all_features(zip_details):
         print(json.dumps(feature), file=tippecanoe.stdin)
     
     tippecanoe.stdin.close()
     tippecanoe.wait()
 
-def stream_all_features(zip_filenames):
+def stream_all_features(zip_details):
     ''' Generate a stream of all locations as GeoJSON features.
     '''
-    for zip_filename in zip_filenames:
+    for (source_base, zip_filename) in zip_details:
+        _L.debug(u'Opening {} ({})'.format(zip_filename, source_base))
+
         zipfile = ZipFile(zip_filename, mode='r')
         for filename in zipfile.namelist():
             # Look for the one expected .csv file in the zip archive.
