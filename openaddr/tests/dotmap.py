827a571f156e63d0fca14e36501005114a77ef23
Michal Migurski <mike@teczno.com>
2015-09-28 09:16:19 -0700

    Updated dotmap tests to match new function definitions

--- openaddr/tests/dotmap.py
+++ openaddr/tests/dotmap.py
@@ -6,7 +6,7 @@ import unittest
 
 import mock
 
-from ..dotmap import get_all_features
+from ..dotmap import stream_all_features
 
 class TestDotmap (unittest.TestCase):
 
@@ -14,14 +14,14 @@ class TestDotmap (unittest.TestCase):
         self.test_dir = mkdtemp()
         self.zipfiles = list()
         
-        self.zipfiles.append(join(self.test_dir, 'file1.zip'))
-        zf = ZipFile(self.zipfiles[-1], 'w')
+        self.zipfiles.append(('us/anytown', join(self.test_dir, 'file1.zip')))
+        zf = ZipFile(self.zipfiles[-1][1], 'w')
         zf.writestr('README.txt', 'Good times')
         zf.writestr('stuff.csv', 'LAT,LON\n0,0\n37.804319,-122.271210\n')
         zf.close()
         
-        self.zipfiles.append(join(self.test_dir, 'file2.zip'))
-        zf = ZipFile(self.zipfiles[-1], 'w')
+        self.zipfiles.append(('us/whoville', join(self.test_dir, 'file2.zip')))
+        zf = ZipFile(self.zipfiles[-1][1], 'w')
         zf.writestr('README.txt', 'Good times')
         zf.writestr('stuff.csv', 'LON,LAT\n0,0\n-122.413729,37.775641\n')
         zf.close()
@@ -29,12 +29,12 @@ class TestDotmap (unittest.TestCase):
     def tearDown(self):
         rmtree(self.test_dir)
 
-    def test_get_all_features_no_runs(self):
-        features = list(get_all_features(self.zipfiles[:0]))
+    def test_stream_all_features_no_runs(self):
+        features = list(stream_all_features(self.zipfiles[:0]))
         self.assertEqual(len(features), 0)
 
-    def test_get_all_features_one_run(self):
-        features = list(get_all_features(self.zipfiles[:1]))
+    def test_stream_all_features_one_run(self):
+        features = list(stream_all_features(self.zipfiles[:1]))
 
         p1, p2 = [f['geometry']['coordinates'] for f in features]
         self.assertAlmostEqual(p1[0],    0.0)
@@ -42,8 +42,8 @@ class TestDotmap (unittest.TestCase):
         self.assertAlmostEqual(p2[0], -122.271210)
         self.assertAlmostEqual(p2[1],   37.804319)
 
-    def test_get_all_features_two_runs(self):
-        features = list(get_all_features(self.zipfiles[:2]))
+    def test_stream_all_features_two_runs(self):
+        features = list(stream_all_features(self.zipfiles[:2]))
 
         p1, p2, p3, p4 = [f['geometry']['coordinates'] for f in features]
         self.assertAlmostEqual(p1[0],    0.0)
