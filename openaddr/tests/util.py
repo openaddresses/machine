ed96f7ae56c7829317b318940207b12a17bf67ab
Michal Migurski <mike@teczno.com>
2015-12-10 08:55:01 -0800

    Added autoscale capacity function and test

--- openaddr/tests/util.py
+++ openaddr/tests/util.py
@@ -7,8 +7,9 @@ import unittest, tempfile, json
 from mimetypes import guess_type
 from urllib.parse import urlparse, parse_qs
 from httmock import HTTMock, response
+from mock import Mock
 
-from .. import util
+from .. import util, __version__
 from ..util.esri2geojson import esri2geojson
 
 class TestUtilities (unittest.TestCase):
@@ -33,6 +34,43 @@ class TestUtilities (unittest.TestCase):
         self.assertEqual(kwargs2['port'], 5432)
         self.assertEqual(kwargs2['database'], 'what')
         self.assertEqual(kwargs2['sslmode'], 'require')
+    
+    def test_autoscale(self):
+        '''
+        '''
+        autoscale, cloudwatch, as_group = Mock(), Mock(), Mock()
+        group_name = 'CI Workers {0}.x'.format(*__version__.split('.'))
+        
+        cloudwatch.get_metric_statistics.return_value = [{}]
+        autoscale.get_all_groups.return_value = [as_group]
+        
+        as_group.desired_capacity = 2
+        util.set_autoscale_capacity(autoscale, cloudwatch, 1)
+        
+        # The right group name was used.
+        autoscale.get_all_groups.assert_called_once_with([group_name])
+        
+        as_group.desired_capacity = 1
+        util.set_autoscale_capacity(autoscale, cloudwatch, 1)
+        
+        as_group.desired_capacity = 0
+        cloudwatch.get_metric_statistics.return_value = [{'Maximum': 0}]
+        util.set_autoscale_capacity(autoscale, cloudwatch, 1)
+        
+        # Conditions haven't yet required a capacity increase.
+        as_group.set_capacity.assert_not_called()
+
+        cloudwatch.get_metric_statistics.return_value = [{'Maximum': 1}]
+        util.set_autoscale_capacity(autoscale, cloudwatch, 1)
+        
+        # Capacity had to be increased to 1.
+        as_group.set_capacity.assert_called_once_with(1)
+
+        as_group.desired_capacity = 1
+        util.set_autoscale_capacity(autoscale, cloudwatch, 2)
+        
+        # Capacity had to be increased to 2.
+        as_group.set_capacity.assert_called_with(2)
 
 class TestEsri2GeoJSON (unittest.TestCase):
     
--- openaddr/util/__init__.py
+++ openaddr/util/__init__.py
@@ -1,4 +1,7 @@
 from urllib.parse import urlparse, parse_qsl
+from datetime import datetime, timedelta
+
+from .. import __version__
 
 def prepare_db_kwargs(dsn):
     '''
@@ -14,3 +17,21 @@ def prepare_db_kwargs(dsn):
         kwargs.update(dict(sslmode=q['sslmode']))
     
     return kwargs
+
+def set_autoscale_capacity(autoscale, cloudwatch, capacity):
+    '''
+    '''
+    span, now = 60 * 60 * 3, datetime.now()
+    start, end = now - timedelta(seconds=span), now
+    args = 'tasks queue', 'openaddr.ci', 'Maximum'
+
+    (measure, ) = cloudwatch.get_metric_statistics(span, start, end, *args)
+
+    group_name = 'CI Workers {0}.x'.format(*__version__.split('.'))
+    (group, ) = autoscale.get_all_groups([group_name])
+    
+    if group.desired_capacity >= capacity:
+        return
+    
+    if measure['Maximum'] > .9:
+        group.set_capacity(capacity)
