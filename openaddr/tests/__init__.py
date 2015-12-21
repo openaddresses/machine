58ffd03bc39319a9eded4f7f5bd2c017e9dc22e5
Michal Migurski <mike@teczno.com>
2015-12-20 22:42:34 -0800

    Added support for housing unit in conform data with tests

--- openaddr/tests/__init__.py
+++ openaddr/tests/__init__.py
@@ -219,6 +219,10 @@ class TestOA (unittest.TestCase):
             self.assertEqual(rows[10]['STREET'], 'Hillsborough Street')
             self.assertEqual(rows[100]['STREET'], '8th Street')
             self.assertEqual(rows[1000]['STREET'], 'Hanover Avenue')
+            self.assertEqual(rows[1]['UNIT'], '')
+            self.assertEqual(rows[10]['UNIT'], '')
+            self.assertEqual(rows[100]['UNIT'], '')
+            self.assertEqual(rows[1000]['UNIT'], '')
 
     def test_single_sf(self):
         ''' Test complete process_one.process on San Francisco sample data.
@@ -261,6 +265,10 @@ class TestOA (unittest.TestCase):
             self.assertEqual(rows[10]['STREET'], 'Golden Gate Avenue')
             self.assertEqual(rows[100]['STREET'], 'Octavia Street')
             self.assertEqual(rows[1000]['STREET'], 'Folsom Street')
+            self.assertEqual(rows[1]['UNIT'], '')
+            self.assertEqual(rows[10]['UNIT'], '')
+            self.assertEqual(rows[100]['UNIT'], '')
+            self.assertEqual(rows[1000]['UNIT'], '')
 
     def test_single_car(self):
         ''' Test complete process_one.process on Carson sample data.
@@ -292,6 +300,7 @@ class TestOA (unittest.TestCase):
             self.assertEqual(5, len(rows))
             self.assertEqual(rows[0]['NUMBER'], '555')
             self.assertEqual(rows[0]['STREET'], 'Carson Street')
+            self.assertEqual(rows[0]['UNIT'], '')
             self.assertEqual(rows[0]['CITY'], 'CARSON, CA')
             self.assertEqual(rows[0]['POSTCODE'], '90745')
             self.assertEqual(rows[0]['DISTRICT'], '')
@@ -454,6 +463,9 @@ class TestOA (unittest.TestCase):
             self.assertEqual(rows[1]['STREET'], 'Dana Street')
             self.assertEqual(rows[10]['STREET'], 'Channing Way')
             self.assertEqual(rows[100]['STREET'], 'Durant Avenue')
+            self.assertEqual(rows[1]['UNIT'], u'')
+            self.assertEqual(rows[10]['UNIT'], u'')
+            self.assertEqual(rows[100]['UNIT'], u'')
 
     def test_single_pl_ds(self):
         ''' Test complete process_one.process on Polish sample data.
@@ -521,6 +533,9 @@ class TestOA (unittest.TestCase):
             self.assertEqual(rows[1]['STREET'], u'Ulica Dolnych Wa\u0142\xf3w  Gliwice')
             self.assertEqual(rows[10]['STREET'], u'Ulica Dolnych Wa\u0142\xf3w  Gliwice')
             self.assertEqual(rows[100]['STREET'], u'Plac Place Inwalid\xf3w Wojennych  Gliwice')
+            self.assertEqual(rows[1]['UNIT'], u'')
+            self.assertEqual(rows[10]['UNIT'], u'')
+            self.assertEqual(rows[100]['UNIT'], u'')
 
     def test_single_jp_f(self):
         ''' Test complete process_one.process on Japanese sample data.
