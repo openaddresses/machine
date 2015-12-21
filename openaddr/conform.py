4f9bc51aa85284e1167a6b1571ef3379f259589a
Michal Migurski <mike@teczno.com>
2015-12-20 20:00:50 -0800

    Applied conform.normalize_ogr_filename_case() in sampling and processing

--- openaddr/conform.py
+++ openaddr/conform.py
@@ -217,7 +217,8 @@ class ExcerptDataTask(object):
         if data_ext in ('.geojson', '.json'):
             data_path = ExcerptDataTask._sample_geojson_file(data_path)
         
-        datasource = ogr.Open(data_path, 0)
+        ogr_data_path = normalize_ogr_filename_case(data_path)
+        datasource = ogr.Open(ogr_data_path, 0)
         layer = datasource.GetLayer()
 
         if not encoding:
@@ -887,7 +888,8 @@ def extract_to_source_csv(source_definition, source_path, extract_path):
     to longitude and latitude in EPSG:4326.
     """
     if source_definition["conform"]["type"] in ("shapefile", "shapefile-polygon", "xml"):
-        ogr_source_to_csv(source_definition, source_path, extract_path)
+        ogr_source_path = normalize_ogr_filename_case(source_path)
+        ogr_source_to_csv(source_definition, ogr_source_path, extract_path)
     elif source_definition["conform"]["type"] == "csv":
         csv_source_to_csv(source_definition, source_path, extract_path)
     elif source_definition["conform"]["type"] == "geojson":
@@ -897,7 +899,8 @@ def extract_to_source_csv(source_definition, source_path, extract_path):
             csv_source_to_csv(source_definition, source_path, extract_path)
         else:
             _L.info("Non-ESRI GeoJSON source found; this code is not well tested.")
-            ogr_source_to_csv(source_definition, source_path, extract_path)
+            ogr_source_path = normalize_ogr_filename_case(source_path)
+            ogr_source_to_csv(source_definition, ogr_source_path, extract_path)
     else:
         raise Exception("Unsupported source type %s" % source_definition["conform"]["type"])
 
