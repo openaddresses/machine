6c4e6c0cc1179868546e7c15325f45f8f23b252d
sudoprime <bryan@unhwildhats.com>
2016-05-16 15:55:39 -0400

    error handling, restructuring, ready to write WKT

--- parse.py
+++ parse.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import csv
 import os
+import re
 import pprint
 import requests
 import zipfile
 import fiona
 from shapely.geometry import MultiPolygon, shape
+from shapely.wkt import dumps
+
+extensions = ['shp']
 
 """Fetch directly to disk, low memory usage"""
 def fetch(url, filepath):
@@ -23,21 +27,34 @@ def unzip(filepath, dest):
     zf.extractall(dest)
 
 def parse_source(source, idx):
-  print('parsing {} [{}]'.format(source[header.index('source')], idx))
+  #print('parsing {} [{}]'.format(source[header.index('source')], idx))
 
-  path = './workspace/{}'.format(idx)
-  if not os.path.exists(path):
-    os.makedirs(path)
+  try:
+    path = './workspace/{}'.format(idx)
+    if not os.path.exists(path):
+      os.makedirs(path)
 
-  if not os.path.isfile(path +'/cache.zip'):
-    cache_url = source[header.index('cache')]
-    fetch(cache_url, path + '/cache.zip')
+    if not os.path.isfile(path +'/cache.zip'):
+      cache_url = source[header.index('cache')]
+      fetch(cache_url, path + '/cache.zip')  # some of these are csv, not zip
 
-  if not os.path.exists(path + '/cached_files'):
-    unzip(path + '/cache.zip', path + '/cached_files')
+    if not os.path.exists(path + '/cached_files'):
+      unzip(path + '/cache.zip', path + '/cached_files')
 
-  with fiona.drivers():
-    return fiona.open(path + '/cached_files/PARCELS.shp')
+    files = os.listdir(path + '/cached_files')
+    shapefile = None
+    for f in files:
+      if re.match('.*\.{}$'.format('|'.join(extensions)), f):
+        shapefile = f
+
+    if shapefile and os.path.isfile(path + '/cached_files/{}'.format(shapefile)):
+      with fiona.drivers():
+        return fiona.open(path + '/cached_files/{}'.format(shapefile))
+    else:
+      return None
+  except Exception as e:
+    #print(e)
+    return None
 
 def geometry_stats():
   geometries = {}
@@ -50,6 +67,17 @@ def geometry_stats():
       geometries[geometry] = 1
 
   print(geometries)
+
+def convert_to_shapely(f):
+  objects = []
+  for obj in f:  # this is a lossy process needed to get shapely to consume this data
+    if 'geometry' in obj and obj['geometry']:  # needed for 'geometry': None
+      if 'type' in obj['geometry']:
+        if obj['geometry']['type'] == 'Polygon':
+          objects.append(obj['geometry'])
+
+  shapes = [shape(obj) for obj in objects]
+  return MultiPolygon(shapes)
 
 if __name__=='__main__':
   state = []
@@ -74,14 +102,13 @@ if __name__=='__main__':
   for source in to_purge:
     state.remove(source)
 
-  f = parse_source(state[4], 4)
-  objects = []
-  for obj in f:  # this is a lossy process needed to get shapely to consume this data
-    if 'geometry' in obj and obj['geometry']:  # needed for 'geometry': None
-      if 'type' in obj['geometry']:
-        if obj['geometry']['type'] == 'Polygon':
-          objects.append(obj['geometry'])
+  succ_count = 0
 
-  shapes = [shape(obj) for obj in objects]
-  p = MultiPolygon(shapes)
-  print(p)
+  for idx in range(0, len(state)):
+    f = parse_source(state[idx], idx)
+    if f:
+      s = convert_to_shapely(f)
+      if s:
+        #print(dumps(s))
+        print("{} passed".format(idx))
+        succ_count += 1
