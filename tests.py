56b03c8572ea95ae235bd1a97f7ceae5a7035ed4
Michal Migurski <mike@teczno.com>
2016-06-26 10:25:32 -0700

    Added single test for utils.to_shapely_obj()

--- tests.py
+++ tests.py
@@ -25,3 +25,10 @@ class TestUtils (unittest.TestCase):
         scraped = utils.scrape_fiona_metadata(obj, 'us/ca/berkeley.json')
         
         self.assertEqual(scraped, {'CITY': 'BERKELEY', 'ID': '055 183213100', 'REGION': None, 'STREET': 'DANA ST', 'NUMBER': '2550', 'DISTRICT': None, 'LON': None, 'LAT': None, 'UNIT': '', 'POSTCODE': '94704'})
+    
+    def test_to_shapely_obj(self):
+        data = fiona.open('data/us/ca/berkeley/Parcels.shp')
+        obj = next(data)
+        shaped = utils.to_shapely_obj(obj)
+        
+        self.assertEqual(str(shaped), 'POLYGON ((565011.5815000003 4190878.635749999, 565007.0689000003 4190904.882000001, 565044.6956500001 4190911.299000001, 565049.5083999997 4190885.2125, 565011.5815000003 4190878.635749999))')
