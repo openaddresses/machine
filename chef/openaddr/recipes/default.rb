65050161182f5ce74f713f7bb5361b3146015d4f
ingalls <nicholas.ingalls@gmail.com>
2016-07-07 09:38:26 -0400

    GDAL@1.11.3

--- chef/openaddr/recipes/default.rb
+++ chef/openaddr/recipes/default.rb
@@ -5,9 +5,21 @@ package 'python-dev'
 package 'libpq-dev'
 package 'memcached'
 package 'libffi-dev'
-package 'gdal-bin'
 package 'libgdal-dev'
+package 'build-essential'
+package 'python-all-dev'
 
 execute "pip install -U ." do
   cwd File.join(File.dirname(__FILE__), '..', '..', '..')
+end
+
+bash 'install_latest_gdal' do
+  code <<-EOH
+    wget 'http://download.osgeo.org/gdal/1.11.3/gdal-1.11.3.tar.gz'
+    tar xvfz gdal-1.11.3.tar.gz
+    cd gdal-1.11.3
+    make
+    sudo make install
+    cd ..
+    EOH
 end
