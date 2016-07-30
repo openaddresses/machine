d1c4f329172343e72d28b8beeaaece778f9ebb74
Michal Migurski <mike@teczno.com>
2016-07-30 13:31:37 -0700

    Fixed locale environment for pip3 under chef

--- chef/openaddr/recipes/default.rb
+++ chef/openaddr/recipes/default.rb
@@ -1,5 +1,5 @@
 execute "pip3 install -U ." do
   cwd File.join(File.dirname(__FILE__), '..', '..', '..')
-  environment({'LC_CTYPE' => "C.UTF-8"})
+  environment({'LC_ALL' => "C.UTF-8"}) # ...for correct encoding in python open()
 end
 
