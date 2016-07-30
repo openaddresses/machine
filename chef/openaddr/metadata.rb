e609d0958a3d332e7348c6e5cd0842839e7e6871
Michal Migurski <mike@teczno.com>
2016-07-30 12:31:48 -0700

    Rearranged chef recipes to be more compatible with CI

---
+++ chef/openaddr/metadata.rb
@@ -0,0 +1,1 @@
+depends 'openaddr-prereqs'
--- chef/openaddr/recipes/default.rb
+++ chef/openaddr/recipes/default.rb
@@ -1,14 +1,5 @@
-package 'python-cairo'
-package 'python-gdal'
-package 'python-pip'
-package 'python-dev'
-package 'libpq-dev'
-package 'memcached'
-package 'libffi-dev'
-package 'gdal-bin'
-package 'libgdal-dev'
-
 execute "pip install -U ." do
   cwd File.join(File.dirname(__FILE__), '..', '..', '..')
+  environment({'LC_CTYPE' => "C.UTF-8"})
 end
 
