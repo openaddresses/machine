f220c604415ed99f3b571d9f3c7fa0a3fcec9c0c
Michal Migurski <mike@teczno.com>
2016-10-11 21:30:32 -0700

    Added missing data_bag_path

--- chef/solo.rb
+++ chef/solo.rb
@@ -3,6 +3,7 @@
 #
 file_cache_path  "/tmp/chef-solo"
 cookbook_path    File.dirname(__FILE__)
+data_bag_path    File.dirname(__FILE__)
 log_level        :info
 log_location     "/var/log/cheflog"
 ssl_verify_mode  :verify_none
