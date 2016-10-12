1cd80c16677e4a84e1de36bdd969e7a08a3896ce
Michal Migurski <mike@teczno.com>
2016-10-11 20:28:16 -0700

    Added crontab entry for enqueue-sources

--- chef/crontab/recipes/default.rb
+++ chef/crontab/recipes/default.rb
@@ -38,6 +38,10 @@ file "/etc/logrotate.d/openaddr_crontab-dotmap" do
     content "/var/log/openaddr_crontab/dotmap.log\n#{rotation}\n"
 end
 
+file "/etc/logrotate.d/openaddr_crontab-enqueue-sources" do
+    content "/var/log/openaddr_crontab/enqueue-sources.log\n#{rotation}\n"
+end
+
 #
 # Place crontab scripts.
 #
@@ -48,6 +52,7 @@ file "/etc/cron.d/openaddr_crontab-collect-extracts" do
     content <<-CRONTAB
 # Archive collection, every other day at 5am UTC (10pm PDT)
 0 5	*/2 * *	#{username}	( \
+  curl -X POST -d '{"text": "Starting new collection zips..."}' "#{slack_url}" -s ; \
   openaddr-run-ec2-command \
   -a "#{aws_access_id}" \
   -s "#{aws_secret_key}" \
@@ -60,7 +65,7 @@ file "/etc/cron.d/openaddr_crontab-collect-extracts" do
     -s "#{aws_secret_key}" \
     --sns-arn "#{aws_sns_arn}" \
     --verbose \
-  && curl -X POST -d '{"text": "Completed <https://#{cname}/latest/set|new collection zips>."}' "#{slack_url}" -s \
+  && curl -X POST -d '{"text": "Completed <https://#{cname}|new collection zips>."}' "#{slack_url}" -s \
   || curl -X POST -d '{"text": "Failed to complete new collection zips."}' "#{slack_url}" -s ) \
   >> /var/log/openaddr_crontab/collect-extracts.log 2>&1
 CRONTAB
@@ -70,6 +75,7 @@ file "/etc/cron.d/openaddr_crontab-dotmap" do
     content <<-CRONTAB
 # Generate OpenAddresses dot map, every third day midnight UTC (5pm PDT)
 0 0	*/3 * *	#{username}	( \
+  curl -X POST -d '{"text": "Starting new dot map..."}' "#{slack_url}" -s ; \
   openaddr-run-ec2-command \
   --role dotmap \
   --instance-type r3.large \
@@ -87,5 +93,21 @@ file "/etc/cron.d/openaddr_crontab-dotmap" do
   && curl -X POST -d '{"text": "Completed <https://openaddresses.io|new dot map>."}' "#{slack_url}" -s \
   || curl -X POST -d '{"text": "Failed to complete new dot map."}' "#{slack_url}" -s ) \
   >> /var/log/openaddr_crontab/dotmap.log 2>&1
+CRONTAB
+end
+
+file "/etc/cron.d/openaddr_crontab-enqueue-sources" do
+    content <<-CRONTAB
+# Enqueue sources, Fridays 11pm UTC (4pm PDT)
+0 23	* * fri	#{username}	( \
+  curl -X POST -d '{"text": "Starting new batch run..."}' "#{slack_url}" -s ; \
+  openaddr-enqueue-sources \
+  -d "#{database_url}" \
+  -a "#{aws_access_id}" \
+  -s "#{aws_secret_key}" \
+  --sns-arn "#{aws_sns_arn}" \
+  && curl -X POST -d '{"text": "Completed <https://#{cname}/latest/set|new batch run>."}' "#{slack_url}" -s \
+  || curl -X POST -d '{"text": "Failed to complete new batch run."}' "#{slack_url}" -s ) \
+  >> /var/log/openaddr_crontab/enqueue-sources.log 2>&1
 CRONTAB
 end
