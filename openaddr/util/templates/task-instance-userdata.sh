0cb2519d87e7fb140d84886b9ae3615cecd05ce4
Michal Migurski <mike@teczno.com>
2016-10-20 22:52:10 -0700

    Bumped to 3.1.3 with fix to remote-run LC_ALL

--- openaddr/util/templates/task-instance-userdata.sh
+++ openaddr/util/templates/task-instance-userdata.sh
@@ -8,7 +8,6 @@ sudo -u ubuntu git fetch origin {version}
 sudo -u ubuntu git rebase FETCH_HEAD
 chef/run.sh {role}
 
-LC_ALL="C.UTF-8"
-{command} 2>&1
+LC_ALL="C.UTF-8" {command} 2>&1
 
 shutdown -h now
