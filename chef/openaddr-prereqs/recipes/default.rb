0b25d8bf167dd1a5c0b5fdaea0ba44dca5e40356
Michal Migurski <mike@teczno.com>
2016-11-07 21:21:53 -0500

    Re-added ppa:ubuntugis and pinned python3-gdal version

--- chef/openaddr-prereqs/recipes/default.rb
+++ chef/openaddr-prereqs/recipes/default.rb
@@ -1,9 +1,18 @@
+package 'python3-gdal' do
+  version '1.11.2+dfsg-1~exp2~trusty'
+end
+
+package 'gdal-bin' do
+  version '1.11.2+dfsg-1~exp2~trusty'
+end
+
+package 'libgdal-dev' do
+  version '1.11.2+dfsg-1~exp2~trusty'
+end
+
 package 'python3-cairo'
-package 'python3-gdal'
 package 'python3-pip'
 package 'python3-dev'
 package 'libpq-dev'
 package 'memcached'
 package 'libffi-dev'
-package 'gdal-bin'
-package 'libgdal-dev'
