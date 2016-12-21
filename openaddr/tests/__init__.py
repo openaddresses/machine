ae50a4df164fa7fd5ef2ac57fb8d64167e26b508
Michal Migurski <mike@teczno.com>
2016-12-20 21:38:40 -0800

    Added failing Kharkiv test

--- openaddr/tests/__init__.py
+++ openaddr/tests/__init__.py
@@ -961,6 +961,51 @@ class TestOA (unittest.TestCase):
             self.assertEqual(rows[11]['STREET'], u'W 28TH DIVISION HWY')
             self.assertEqual(rows[21]['STREET'], u'W 28TH DIVISION HWY')
 
+    def test_single_ua_kharkiv(self):
+        ''' Test complete process_one.process on data with ESRI multiPolyline geometries.
+        '''
+        source = join(self.src_dir, 'ua-63-city_of_kharkiv.json')
+
+        with HTTMock(self.response_content):
+            state_path = process_one.process(source, self.testdir, False)
+
+        with open(state_path) as file:
+            state = dict(zip(*json.load(file)))
+
+        with open(join(dirname(state_path), state['output'])) as file:
+            print(file.read())
+
+        self.assertIsNotNone(state['sample'])
+        self.assertIsNone(state['preview'])
+
+        with open(join(dirname(state_path), state['sample'])) as file:
+            sample_data = json.load(file)
+
+        self.assertEqual(len(sample_data), 6)
+        self.assertIn('OA:geom', sample_data[0])
+        self.assertIn('UNITNUM', sample_data[0])
+        self.assertEqual('423', sample_data[1][0])
+        self.assertEqual(['W', ' ', '28TH DIVISION', 'HWY'], sample_data[1][1:5])
+        self.assertEqual('1', sample_data[1][6])
+        self.assertEqual('2', sample_data[2][6])
+        self.assertEqual('3', sample_data[3][6])
+        self.assertEqual('4', sample_data[4][6])
+        self.assertEqual('5', sample_data[5][6])
+        
+        output_path = join(dirname(state_path), state['processed'])
+        
+        with csvopen(output_path, encoding='utf8') as input:
+            rows = list(csvDictReader(input, encoding='utf8'))
+            self.assertEqual(rows[1]['UNIT'], u'2')
+            self.assertEqual(rows[11]['UNIT'], u'11')
+            self.assertEqual(rows[21]['UNIT'], u'')
+            self.assertEqual(rows[1]['NUMBER'], u'423')
+            self.assertEqual(rows[11]['NUMBER'], u'423')
+            self.assertEqual(rows[21]['NUMBER'], u'7')
+            self.assertEqual(rows[1]['STREET'], u'W 28TH DIVISION HWY')
+            self.assertEqual(rows[11]['STREET'], u'W 28TH DIVISION HWY')
+            self.assertEqual(rows[21]['STREET'], u'W 28TH DIVISION HWY')
+
     def test_single_pa_bucks(self):
         ''' Test complete process_one.process on data with ESRI multiPolyline geometries.
         '''
