0646e79b8148eee577e529aa9b5efd2ef526465b
Michal Migurski <mike@teczno.com>
2017-01-03 21:39:33 -0800

    Removed UTF8-encoded Toscana test to leave only the real one

--- openaddr/tests/__init__.py
+++ openaddr/tests/__init__.py
@@ -249,9 +249,6 @@ class TestOA (unittest.TestCase):
         if (host, path) == ('s3.amazonaws.com', '/data.openaddresses.io/cache/uploads/migurski/ed789f/toscana20160804.zip'):
             local_path = join(data_dirname, 'it-52-statewide.zip')
         
-        if (host, path) == ('s3.amazonaws.com', '/data.openaddresses.io/cache/uploads/migurski/ed789f/toscana20160804-utf8.zip'):
-            local_path = join(data_dirname, 'it-52-statewide-utf8.zip')
-        
         if (host, path) == ('njgin.state.nj.us', '/download2/Address/ADDR_POINT_NJ_fgdb.zip'):
             local_path = join(data_dirname, 'nj-statewide.gdb.zip')
         
@@ -1322,35 +1319,6 @@ class TestOA (unittest.TestCase):
         ''' Test complete process_one.process on data.
         '''
         source = join(self.src_dir, 'it-52-statewide.json')
-
-        with HTTMock(self.response_content):
-            state_path = process_one.process(source, self.testdir, False)
-
-        with open(state_path) as file:
-            state = dict(zip(*json.load(file)))
-
-        output_path = join(dirname(state_path), state['processed'])
-        
-        with csvopen(output_path, encoding='utf8') as input:
-            rows = list(csvDictReader(input, encoding='utf8'))
-            self.assertEqual(len(rows), 19)
-            self.assertEqual(rows[0]['NUMBER'], u'33')
-            self.assertEqual(rows[0]['STREET'], u'VIA CARLO CARRÀ')
-            self.assertEqual(rows[1]['NUMBER'], u'23')
-            self.assertEqual(rows[1]['STREET'], u'VIA CARLO CARRÀ')
-            self.assertEqual(rows[2]['NUMBER'], u'2')
-            self.assertEqual(rows[2]['STREET'], u'VIA MARINO MARINI')
-            self.assertAlmostEqual(float(rows[0]['LON']), 10.1866102, places=5)
-            self.assertAlmostEqual(float(rows[0]['LAT']), 43.9556084, places=5)
-            self.assertAlmostEqual(float(rows[1]['LON']), 10.1858962, places=5)
-            self.assertAlmostEqual(float(rows[1]['LAT']), 43.9551594, places=5)
-            self.assertAlmostEqual(float(rows[2]['LON']), 10.1863462, places=5)
-            self.assertAlmostEqual(float(rows[2]['LAT']), 43.9547064, places=5)
-
-    def test_single_it_52_statewide_utf8(self):
-        ''' Test complete process_one.process on data.
-        '''
-        source = join(self.src_dir, 'it-52-statewide-utf8.json')
 
         with HTTMock(self.response_content):
             state_path = process_one.process(source, self.testdir, False)
