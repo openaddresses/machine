216be98ad5d21bef2d57a68cf5fb6c9a3847fed1
Michal Migurski <mike@teczno.com>
2017-02-09 17:33:06 -0800

    Removed remaining Slack webhook from recipes and configuration

--- chef/worker/recipes/default.rb
+++ chef/worker/recipes/default.rb
@@ -13,49 +13,6 @@ gag_github_status = local['gag_github_status']
 database_url = "postgres://#{db_user}:#{db_pass}@#{db_host}/#{db_name}?sslmode=require"
 web_docroot = local['web_docroot']
 mapzen_key = local['mapzen_key']
-slack_url = local['slack_url']
-
-#
-# Announce status to the world.
-#
-file '/etc/init.d/openaddr-worker' do
-  mode '0755'
-  content <<-INITD
-#! /bin/sh
-### BEGIN INIT INFO
-# Provides:          openaddr-worker
-# Required-Start:    $local_fs $network
-# Required-Stop:     $local_fs $network
-# Should-Start:      $named
-# Should-Stop:       $named
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start OpenAddresses worker.
-# Description:       Start OpenAddresses worker.
-### END INIT INFO
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-
-case "$1" in
-  start)
-    curl -X POST -d '{"text": "Worker started on `'`hostname --long`'`."}' '#{slack_url}'
-    ;;
-  stop)
-    curl -X POST -d '{"text": "Worker stopped on `'`hostname --long`'`."}' '#{slack_url}'
-    ;;
-  restart|reload|force-reload)
-    echo "Error: argument '$1' not supported" >&2
-    exit 3
-    ;;
-  *)
-    echo "Usage: $0 start|stop" >&2
-    exit 3
-    ;;
-esac
-INITD
-end
-
-execute "update-rc.d openaddr-worker defaults"
-execute "/etc/init.d/openaddr-worker start"
 
 env_file = "/tmp/#{app_name}.conf"
 procfile = File.join(File.dirname(__FILE__), '..', '..', 'Procfile-worker')
