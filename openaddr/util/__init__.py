d1195aeb92be5ca55f9a51e5b16150326e4eac1a
Michal Migurski <mike@teczno.com>
2017-05-03 20:35:00 -0700

    Removed openaddr-run-ec2-command command and util.request_task_instance()
    This functionality has moved to ops/run-ec2-command.py and AWS Lambda.

--- openaddr/util/__init__.py
+++ openaddr/util/__init__.py
@@ -12,9 +12,6 @@ import io, zipfile
 import json, time
 import shlex, re
 
-from boto.exception import EC2ResponseError
-from boto.ec2 import blockdevicemapping
-
 RESOURCE_LOG_INTERVAL = timedelta(seconds=30)
 RESOURCE_LOG_FORMAT = 'Resource usage: {{ user: {user:.0f}%, system: {system:.0f}%, ' \
     'memory: {memory:.0f}MB, read: {read:.0f}KB, written: {written:.0f}KB, ' \
@@ -59,71 +56,6 @@ def set_autoscale_capacity(autoscale, cloudwatch, cloudwatch_ns, capacity):
     
     if measure['Maximum'] > .9:
         group.set_capacity(capacity)
-
-def request_task_instance(ec2, autoscale, instance_type, chef_role, lifespan, command, bucket, aws_sns_arn, tempsize=None):
-    '''
-    '''
-    group_name = 'CI Workers {0}.x'.format(*get_version().split('.'))
-
-    (group, ) = autoscale.get_all_groups([group_name])
-    (config, ) = autoscale.get_all_launch_configurations(names=[group.launch_config_name])
-    (image, ) = ec2.get_all_images(image_ids=[config.image_id])
-    keypair = [kp for kp in ec2.get_all_key_pairs() if kp.name.startswith('oa-')][0]
-
-    yyyymmdd = datetime.utcnow().strftime('%Y-%m-%d-%H-%M')
-    
-    with open(join(dirname(__file__), 'templates', 'task-instance-userdata.sh')) as file:
-        userdata_kwargs = dict(
-            role = shlex.quote(chef_role),
-            command = ' '.join(map(shlex.quote, command)),
-            lifespan = shlex.quote(str(lifespan)),
-            version = shlex.quote(get_version()),
-            log_prefix = shlex.quote('logs/{}-{}'.format(yyyymmdd, command[0])),
-            bucket = shlex.quote(bucket or 'data.openaddresses.io'),
-            aws_sns_arn = '', aws_region = '',
-            command_name = command[0]
-            )
-        
-        if aws_sns_arn:
-            try:
-                _, _, _, aws_region, _ = aws_sns_arn.split(':', 4)
-            except ValueError:
-                pass
-            else:
-                if aws_sns_arn.startswith('arn:aws:sns:'):
-                    userdata_kwargs.update(aws_sns_arn = shlex.quote(aws_sns_arn),
-                                           aws_region = shlex.quote(aws_region))
-    
-        device_map = blockdevicemapping.BlockDeviceMapping()
-
-        if tempsize:
-            dev_sdb = blockdevicemapping.BlockDeviceType(delete_on_termination=True)
-            dev_sdb.size = tempsize
-            device_map['/dev/sdb'] = dev_sdb
-
-        run_kwargs = dict(instance_type=instance_type, security_groups=['default'],
-                          instance_initiated_shutdown_behavior='terminate',
-                          user_data=file.read().format(**userdata_kwargs),
-                          # TODO: use current role from http://169.254.169.254/latest/meta-data/iam/info
-                          instance_profile_name='machine-communication',
-                          key_name=keypair.name, block_device_map=device_map)
-        
-    reservation = image.run(**run_kwargs)
-    (instance, ) = reservation.instances
-    
-    try:
-        instance.add_tag('Name', 'Scheduled {} {}'.format(yyyymmdd, command[0]))
-    except EC2ResponseError:
-        time.sleep(10)
-        try:
-            instance.add_tag('Name', 'Scheduled {} {}'.format(yyyymmdd, command[0]))
-        except EC2ResponseError:
-            time.sleep(10)
-            instance.add_tag('Name', 'Scheduled {} {}'.format(yyyymmdd, command[0]))
-    
-    _L.info('Started EC2 instance {} from AMI {}'.format(instance, image))
-    
-    return instance
 
 def package_output(source, processed_path, website, license):
     ''' Write a zip archive to temp dir with processed data and optional .vrt.
