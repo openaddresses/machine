def7516fd09c294ee3ba3a5d3153f9a31a7d23c2
ingalls <ingalls@protonmail.com>
2018-03-11 21:42:35 -0400

    Clean up formatting

--- openaddr/__init__.py
+++ openaddr/__init__.py
@@ -43,22 +43,22 @@ class S3:
     def __init__(self, key, secret, bucketname):
         self._key, self._secret = key, secret
         self.bucketname = bucketname
-    
+
     def _make_bucket(self):
         if not self._bucket:
             # see https://github.com/boto/boto/issues/2836#issuecomment-67896932
             kwargs = dict(calling_format='boto.s3.connection.OrdinaryCallingFormat')
             connection = S3Connection(self._key, self._secret, **kwargs)
             self._bucket = connection.get_bucket(self.bucketname)
-    
+
     @property
     def bucket(self):
         self._make_bucket()
         return self._bucket
-    
+
     def get_key(self, name):
         return self.bucket.get_key(name)
-    
+
     def new_key(self, name):
         return self.bucket.new_key(name)
 
@@ -66,7 +66,7 @@ class LocalProcessedResult:
     def __init__(self, source_base, filename, run_state, code_version):
         for attr in ('attribution_name', 'attribution_flag', 'website', 'license'):
             assert hasattr(run_state, attr), 'Run state should have {} property'.format(attr)
-        
+
         self.source_base = source_base
         self.filename = filename
         self.run_state = run_state
@@ -74,7 +74,7 @@ class LocalProcessedResult:
 
 def cache(data_source_name, data_source, destdir, extras):
     ''' Python wrapper for openaddress-cache.
-    
+
         Return a CacheResult object:
 
           cache: URL of cached data, possibly with file:// schema
@@ -82,14 +82,14 @@ def cache(data_source_name, data_source, destdir, extras):
           version: data version as date?
           elapsed: elapsed time as timedelta object
           output: subprocess output as string
-        
+
         Creates and destroys a subdirectory in destdir.
     '''
     start = datetime.now()
     workdir = mkdtemp(prefix='cache-', dir=destdir)
-    
+
     data_source.update(extras)
-    
+
     #
     #
     #
@@ -106,7 +106,7 @@ def cache(data_source_name, data_source, destdir, extras):
     # we should zip them together before uploading to S3 instead of picking
     # the first one only.
     filepath_to_upload = abspath(downloaded_files[0])
-    
+
     #
     # Find the cached data and hold on to it.
     #
@@ -123,7 +123,7 @@ def cache(data_source_name, data_source, destdir, extras):
 
 def conform(data_source_name, data_source, destdir, extras):
     ''' Python wrapper for openaddresses-conform.
-    
+
         Return a ConformResult object:
 
           processed: URL of processed data CSV
@@ -131,14 +131,14 @@ def conform(data_source_name, data_source, destdir, extras):
           geometry_type: typically Point or Polygon
           elapsed: elapsed time as timedelta object
           output: subprocess output as string
-        
+
         Creates and destroys a subdirectory in destdir.
     '''
     start = datetime.now()
     workdir = mkdtemp(prefix='conform-', dir=destdir)
-    
+
     data_source.update(extras)
-    
+
     #
     # The cached data will be a local path.
     #
@@ -187,7 +187,7 @@ def conform(data_source_name, data_source, destdir, extras):
         out_path = realpath(join(destdir, 'out.csv'))
 
     rmtree(workdir)
-    
+
     sharealike_flag = conform_sharealike(data_source.get('license'))
     attr_flag, attr_name = conform_attribution(data_source.get('license'), data_source.get('attribution'))
 
@@ -205,22 +205,22 @@ def conform(data_source_name, data_source, destdir, extras):
 
 def iterate_local_processed_files(runs, sort_on='datetime_tz'):
     ''' Yield a stream of local processed result files for a list of runs.
-    
+
         Used in ci.collect and dotmap processes.
     '''
     if sort_on == 'source_path':
         reverse, key = False, lambda run: run.source_path
     else:
         reverse, key = True, lambda run: run.datetime_tz or date(1970, 1, 1)
-    
+
     for run in sorted(runs, key=key, reverse=reverse):
         source_base, _ = splitext(relpath(run.source_path, 'sources'))
         processed_url = run.state and run.state.processed
         run_state = run.state
-    
+
         if not processed_url:
             continue
-        
+
         try:
             filename = download_processed_file(processed_url)
         except:
@@ -234,29 +234,29 @@ def iterate_local_processed_files(runs, sort_on='datetime_tz'):
                 except:
                     _L.error('Failed to download {}'.format(processed_url))
                     continue
-        
+
         yield LocalProcessedResult(source_base, filename, run_state, run.code_version)
 
         if filename and exists(filename):
             remove(filename)
-    
+
 def download_processed_file(url):
     ''' Download a URL to a local temporary file, return its path.
-    
+
         Local file will have an appropriate timestamp and extension.
     '''
     _, ext = splitext(urlparse(url).path)
     handle, filename = mkstemp(prefix='processed-', suffix=ext)
     close(handle)
-    
+
     response = get(url, stream=True, timeout=5)
-    
+
     with open(filename, 'wb') as file:
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
-    
+
     last_modified = response.headers.get('Last-Modified')
     timestamp = timegm(parse(last_modified).utctimetuple())
     utime(filename, (timestamp, timestamp))
-    
+
     return filename
